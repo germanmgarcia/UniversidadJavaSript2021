e65cd0dda48a1e314149e17c6e9d3ef953434c18
Germán <gmgarcia@ucundinamarca.edu.co>
2021-06-16 16:41:44 -0500

    Reglas para definir nombres de Variables en JavaScript

---
+++ Leccion02/02-05-ReglasNombresVariables.js
@@ -0,0 +1,11 @@
+let nombreCompleto = "Germán García";
+let nombrecompleto = "Carlos Gomez";
+console.log(nombreCompleto);
+console.log(nombrecompleto);
+
+
+//No podemos declara una variable empezando por un número
+let a1nombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+//let 1nombreCompleto; No esta permitido iniciar el nombre de una variable con número
