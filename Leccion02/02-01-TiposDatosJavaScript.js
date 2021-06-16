1a6a89a4943f9a91cb41a3e0ff98e9ac2834d2bc
Germán <mgarcia@identica-sa.com>
2021-06-16 12:44:02 -0500

    Más de tipos de Datos en JavaScript

--- Leccion02/02-01-TiposDatosJavaScript.js
+++ Leccion02/02-01-TiposDatosJavaScript.js
@@ -11,6 +11,7 @@ console.log(typeof nombre);
 //Tipo de dato numerico
 var numero = 1000;
 console.log(numero);
+
 //Tipo de dato objeto
 var objeto = {
   nombre: "Juan",
@@ -18,3 +19,35 @@ var objeto = {
   telefono: "55448844",
 };
 console.log(typeof objeto);
+
+//Tipo de dato booleano (true, false)
+var bandera = false;
+console.log(bandera);
+
+//Tipo de dato función
+function miFuncion() {}
+console.log(typeof miFuncion);
+
+//Tipo de dato Symbol
+var simbolo = Symbol("mi simbolo");
+console.log(typeof simbolo);
+
+//Tipo clase es una función
+class Persona {
+  constructor(nombre, apellido) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+  }
+}
+console.log(typeof Persona);
+
+//Tipo undefined
+var x;
+console.log(x);
+
+x = undefined;
+console.log(typeof x);
+
+//null = ausencia de valor
+var y = null;
+console.log(typeof y);
