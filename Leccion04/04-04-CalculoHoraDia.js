8eb84bb0800a401ad2438eba61abdb9ac89992e2
Germán <gmgarcia@ucundinamarca.edu.co>
2021-07-05 19:09:10 -0500

    Ejercicio Cálculo Hora del Día

---
+++ Leccion04/04-04-CalculoHoraDia.js
@@ -0,0 +1,22 @@
+/**
+ * 6am-11am - Buenos dias
+ * 12pm-18pm - Buenas tardes
+ * 19pm-24pm - Buenas noches
+ * 0am-6am - Durmiendo
+ */
+
+let horaDia = 17;
+let mensaje;
+
+if (horaDia >= 6 && horaDia <= 11) {
+  mensaje = "Buenos dias";
+} else if (horaDia >= 12 && horaDia <= 18) {
+  mensaje = "Buenas tardes";
+} else if (horaDia >= 19 && horaDia <= 24) {
+  mensaje = "Buenas noches";
+} else if (horaDia >= 0 && horaDia < 6) {
+  mensaje = "Durmiendo";
+} else {
+  mensaje = "Valor incorrecto";
+}
+console.log(mensaje);
